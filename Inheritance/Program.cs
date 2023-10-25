7352810f1afd3ac4b6c55617939ca794f771f822
Buse Yıldız <busenurryildiz@hotmail.com>
2023-10-26 01:28:24 +0300

    C# Dersleri 47

---
+++ Inheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName = "Buse"
+                },
+                new Student
+                {
+                    FirstName = "Belinay"
+                },
+                new Person
+                {
+                    FirstName = "Burak"
+                }
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);   
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
+
+    }
+}
